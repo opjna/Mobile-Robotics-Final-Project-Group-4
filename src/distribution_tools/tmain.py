263ef2fab79010fb43fefd4fca3c1fc5e46b3563
Ryan Lewis <accounts+github@ryandlewis.dev>
2022-04-09 16:29:42 -0400

    More distributions, skew calculation

--- src/distribution_tools/tmain.py
+++ src/distribution_tools/tmain.py
@@ -1,12 +1,14 @@
 from scipy.stats import norm, expon, gamma, weibull_min, rayleigh, beta, binom, poisson, geom
+# import matplotlib
 import matplotlib.pyplot as plt; plt.ion()
+# matplotlib.use('TkAgg')
 
 from scipy.optimize import minimize
 from tskew.tskew import getObjectiveFunction, tspdf_1d, tskew_moments
 
 import numpy as np
 
-def gen_data_poly(N = 1000):
+def gen_data_poly(N = 2000):
     # Normal
     nmean=1
     nloc=2
@@ -24,17 +26,36 @@ def gen_data_poly(N = 1000):
     gamma_data = gamma.rvs(a, scale=2, size = N)
     gamma_dist = gamma(a, scale=2)
     # Weibull
-    # Rayleigh
-    # Beta
+    x = 1
+
+    # Rayleigh - TODO
+    rayleigh_data = rayleigh.rvs(loc=nmean, scale=nloc, size = N)
+    rayleigh_dist = rayleigh(loc=nmean, scale=nloc)
+    # Beta - TODO
+    beta_a=3
+    beta_b=4
+    beta_data = beta.rvs(a=beta_a, b=beta_b, scale = 2, size = N)
+    beta_dist = beta(a=beta_a, b=beta_b, scale=2)
     # Binomial
-    # Poisson
+    # n = 10
+    # p = .1
+    # binom_data = binom.rvs(n=n, p=p, loc=1, size=N)
+    # binom_dist = binom(n=n, p=p, loc=1)
+    # Poisson - TODO
+    # mu_poisson = 2
+    # poisson_data = poisson.rvs(mu=mu_poisson, size = N)
+    # poisson_dist = poisson(mu=mu_poisson)
     # Geometric
     # Negative Binomial
 
     data_dict = {
         'Normal': ([nmean, nloc], normal_data, normal_dist),
         'Exponential': ([lam], exponential_data, exponential_dist),
-        'Gamma': ([a,b], gamma_data, gamma_dist)
+        'Gamma': ([a,b], gamma_data, gamma_dist),
+        'Rayleigh': ([nmean, nloc], rayleigh_data, rayleigh_dist),
+        'Beta': ([beta_a, beta_b], beta_data, beta_dist),
+        # 'Poisson': ([mu_poisson], poisson_data, poisson_dist),
+        # 'Binomial': ([n, p], binom_data, binom_dist)
     }
     return data_dict
 
@@ -48,8 +69,9 @@ if __name__ == "__main__":
 
         loc = np.mean(realization)
         scale = np.var(realization)
+        median = np.median(realization)
         df = 1000
-        skew = 0
+        skew = (3 * loc - median) / np.sqrt(scale)
 
         theta = np.array([loc, scale, df, skew])
 
@@ -67,6 +89,7 @@ if __name__ == "__main__":
         plt.plot(xvals, est_pdf, linestyle='--', label='Estimated skew t', linewidth=5)
         plt.title(f'{dist_name}-distributed data and estimated skew ' + r'$t$-distribution')
         plt.legend()
+        plt.show()
 
         solution_params = res['x']
         loc_est, scale_est, df_est, skew_est = solution_params
