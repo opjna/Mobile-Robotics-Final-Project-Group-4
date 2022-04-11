98741755943068c7b5743a6c3ddccfe8983cacc6
Erick Vega <efvega@umich.edu>
2022-04-11 18:31:21 -0400

    Minor updates to inverse CDF code for t skew

--- src/distribution_tools/model_imu_data.py
+++ src/distribution_tools/model_imu_data.py
@@ -4,7 +4,7 @@ import scipy.stats as stats
 
 from scipy.optimize import minimize
 from tskew.tskew import getObjectiveFunction, tspdf_1d
-
+from tskew.tskew import ts_invcdf
 
 import pyreadr
 
@@ -30,16 +30,26 @@ df = 1000
 skew = 0
 
 theta = np.array([loc, scale, df, skew])
-
-res = minimize(getObjectiveFunction(realization, use_loglikelihood=True), x0=theta,
-               method='Nelder-Mead')
+#
+# res = minimize(getObjectiveFunction(realization, use_loglikelihood=True), x0=theta,
+#                method='Nelder-Mead')
 
 N = 1_000
-extent =  np.max(realization) - np.min(realization)
-xvals = np.linspace(np.min(realization) - 0.1 * extent, np.max(realization) + 0.1 * extent, N)
+xmin = -0.05
+xmax = 0.05
+extent =  xmax - xmin
+xvals = np.linspace(xmin - 0.1 * extent, xmax + 0.1 * extent, N)
 
-plt.figure()
-est_pdf = tspdf_1d(xvals, res.x[0], res.x[1], res.x[2], res.x[3])
-plt.hist(realization, bins=100, density=True)
-plt.plot(xvals, est_pdf, linestyle='--', label='Estimated skew t', linewidth=4)
-plt.legend()
+# plt.figure()
+# est_pdf = tspdf_1d(xvals, res.x[0], res.x[1], res.x[2], res.x[3])
+# plt.hist(realization, bins=500, density=True, color='green', alpha=0.5)
+# plt.plot(xvals, est_pdf, linestyle='--', label='Estimated skew t', linewidth=3, alpha=0.5)
+# plt.xlim([xmin, xmax])
+# plt.legend()
+
+loc = 2.72e-5
+scale = 2.25e-6
+df = 1
+skew = 2.8e-3
+
+# median = ts_invcdf(np.array([0.25, 0.5, 0.75]), loc, scale, df, skew)
